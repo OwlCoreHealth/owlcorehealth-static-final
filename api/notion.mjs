c3996d678a72cadb28c652145a81532c1d01e689
OwlCoreHealth <joaopaulocrbatista@gmail.com>
2025-05-23 12:37:57 +0100

    Update notion.mjs

--- api/notion.mjs
+++ api/notion.mjs
@@ -7,7 +7,7 @@ const notion = new Client({
   auth: "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"
 });
 
-const databaseId = "1faa050ee113805e8f1bd34a11ce013f";
+const databaseId = "1fca050ee11380c7ac71ff9556793306";
 
 // 🔍 Extração de palavras-chave
 function extractKeywords(text) {
