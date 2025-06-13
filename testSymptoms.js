47639987f941a0c9240196a116d36949dd1678ec
OwlCoreHealth <joaopaulocrbatista@gmail.com>
2025-06-13 09:51:18 +0100

    Update testSymptoms.js

--- testSymptoms.js
+++ testSymptoms.js
@@ -1,6 +1,12 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import { getAllSymptoms } from "./api/notion.mjs";
 
 (async () => {
+  console.log("NOTION_API_KEY:", process.env.NOTION_API_KEY);
+  console.log("NOTION_DATABASE_ID:", process.env.NOTION_DATABASE_ID);
+
   const sintomas = await getAllSymptoms();
   console.log("Lista de sintomas do Notion:", sintomas);
 })();
