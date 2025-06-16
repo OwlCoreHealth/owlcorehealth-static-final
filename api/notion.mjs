cd4a856ff33069c48a47ce3fb936be0e2c78c5cf
OwlCoreHealth <joaopaulocrbatista@gmail.com>
2025-06-16 21:22:03 +0100

    Update notion.mjs

--- api/notion.mjs
+++ api/notion.mjs
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config(); 
 
-import { Client } from "@notionhq/client";
+import { Client } from "@notionhq/client"; 
 
 // Função utilitária para extrair texto da propriedade Notion
 function getTextFromProperty(prop) {
